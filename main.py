1c98ab0c8f88e7d06e3bfc91ebe6d27c6940e5e4
Edhyjox <edhyjox@gmail.com>
2021-05-03 23:58:31 +0200

    working module

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    pass
+def convert(n: int) -> int:
+    if n < 10:
+        return n
+
+    return sum(divmod(n, 10))
 
 
-if __name__ == '__main__':
-    main()
+def check(card) -> bool:
+    card_digits = [int(x) for x in str(card) if x not in "-_ "]
+    checksum = card_digits.pop()
+    key = sum(convert(digit * (((c + 1) % 2) + 1)) for c, digit in enumerate(card_digits))
+
+    return not (key + checksum) % 10
