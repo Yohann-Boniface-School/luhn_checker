e80cd83c275819f0e4ce5670542e8d66f7f95e19
Edhyjox <edhyjox@gmail.com>
2021-05-04 00:02:54 +0200

    Typing & docstrings

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from typing import Union, Iterable
+
+
 def convert(n: int) -> int:
-    if n < 10:
-        return n
+    """ 
+    An inner function that convert two digit integer into single digit.
+    :param n: integer to convert
+    :return: converted integer
+    """
+    return n if n < 10 else sum(divmod(n, 10))
 
-    return sum(divmod(n, 10))
 
-
-def check(card) -> bool:
+def check(card: Union[int, Iterable[Union[int, str]]]) -> bool:
+    """
+    Check the validity of a given card
+    :param card: the number of the card in a integer or string value
+    :return:
+    """
     card_digits = [int(x) for x in str(card) if x not in "-_ "]
     checksum = card_digits.pop()
     key = sum(convert(digit * (((c + 1) % 2) + 1)) for c, digit in enumerate(card_digits))
